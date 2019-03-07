1543661c45feb04f1648b27733d793a38ff7d135
redouan <redouan778@gmail.com>
2019-03-07 12:33:27 +0100

    fixed the lay-out and 2 extra features
    - The lay-out has been improved.
    -The size and slicing function those are finished.
    -Cleaned up the code.

--- pizzaScript.js
+++ pizzaScript.js
@@ -3,7 +3,7 @@ var pizzaName = document.getElementById('pizzaName');
 var pizzaImage = document.getElementById('pizzaImage');
 var pizzaPrice = document.getElementById('pizzaPrice');
 var slicingAlreadySelected = false;
-var SelectedSize = null;
+var selectedSize = null;
 var sizeinUse = false;
 var toppingIsSelected = false;
 var alert = document.getElementById('alert');
@@ -137,10 +137,10 @@ for (var i = 0; i < pizzas.length; i++) {
     }
 
 
-    var pmar = document.getElementById('p0');
-    var pkeb = document.getElementById('p1');
-    var pfun = document.getElementById('p2');
-    var ppol = document.getElementById('p3');
+    var mar = document.getElementById('p0');
+    var keb = document.getElementById('p1');
+    var fun = document.getElementById('p2');
+    var pol = document.getElementById('p3');
 
 var toppinglist = document.getElementById('toppingNameList');
 
@@ -153,3 +153,67 @@ for (var i = 0; i < toppings.length; i++) {
   toppinglist.appendChild(item);
 
 }
+
+var toppinglist = document.getElementById('toppingNameList');
+
+for (var i = 0; i < toppings.length; i++) {
+  // Create the list item:
+  var item = document.createElement('li');
+
+  // Set its contents:
+  item.appendChild(document.createTextNode(toppings[i].Name));
+
+  //add id to the topping
+  item.setAttribute("id" , "t" + i);
+  // Add it to the list:
+  toppinglist.appendChild(item);
+
+}
+
+
+var tche = document.getElementById('t0');
+var tchi = document.getElementById('t1');
+var tbbq = document.getElementById('t2');
+var toli = document.getElementById('t3');
+
+
+//retrieving sizwe
+var sizelist = document.getElementById('sizeNameList');
+
+    for (var i = 0; i < size.length; i++) {
+    // Create the list item:
+    var item = document.createElement('li');
+
+    // Set its contents:
+    item.appendChild(document.createTextNode(size[i].Name));
+
+    // Add it to the list:
+    sizelist.appendChild(item);
+
+    //add id to the topping
+    item.setAttribute("id" , "s" + i)
+    }
+
+    //get all toppings by id
+    var snor = document.getElementById('s0');
+    var smed = document.getElementById('s1');
+    var slar = document.getElementById('s2');
+    var skin = document.getElementById('s3');
+
+
+
+
+
+
+    function selectSlicing(Selected) {
+    // loop over alles in de slicing array
+    // kijk of de naam overeenkomst met 'Selected'
+    // zo ja, zet dan daar "Selected" op true in de array
+    var i;
+    for (i = 0; i < slicing.length; i++) {
+      if(slicing[i].Name == Selected) {
+        slicing[i].Selected = true;
+
+            //get all toppings by id
+            var snor = document.getElementById('s0');
+            var smed = document.getElementById('s1');
